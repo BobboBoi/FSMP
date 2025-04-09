28958b37c1d9970b27c2bd5ef4854a05a237690b
BobboBoi <jurre.serrarens@student.graafschapcollege.nl>
2025-04-09 12:10:51 +0200

    Why it not commit >:(

--- Scripts/Components/HomeMenu/Tabs/AlbumTab.gd
+++ Scripts/Components/HomeMenu/Tabs/AlbumTab.gd
@@ -11,16 +11,14 @@ func _OnTabClosed():
 func _OnTabOpened():
 	for i in list.get_children(): i.free()
 	
-	var numb = 0
 	#List Albums
 	for i in Lister.albums:
-		var butt := await AlbumSelection.Create(i, await Lister.LoadAlbumCover(i))
+		var butt := AlbumSelection.Create(i)
 		list.add_child(butt)
 		butt.ConnectToAlbum(home)
 		
 		#Connect selection signals
 		butt.Selected.connect(home.SelectedItem.bind(butt),CONNECT_DEFERRED)
 		butt.Unselected.connect(home.UnselectedItem.bind(butt),CONNECT_DEFERRED)
-		list.Update()
-		
-		numb += 1
+	
+	list.Update()
