265db7d88a0d7fc50b387289a949cd1b4308015d
Bobbo <github.jurre@serrarens.nl>
2026-03-23 22:49:58 +0100

    Add flac support using gdextension :D

--- Scripts/MusicPlayer.gd
+++ Scripts/MusicPlayer.gd
@@ -71,20 +71,24 @@ func PlayNewTrack(music : String,trackName : String = "",album : String = "",art
 	
 	print(music)
 	currentPath = music
-	var file = FileAccess.open(music, FileAccess.READ)
-	var bytes = file.get_buffer(file.get_length())
+	if !music.ends_with(".flac"):
+		var file = FileAccess.open(music, FileAccess.READ)
+		var bytes = file.get_buffer(file.get_length())
+		
+		# Load mp3
+		if music.ends_with(".mp3"):
+			self.stream = StreamLoader.LoadMP3FromBytes(bytes)
+		# Load wav
+		elif music.ends_with(".wav"):
+			self.stream = StreamLoader.LoadWAVFromBytes(bytes)
+		# Load wav
+		elif music.ends_with(".ogg"):
+			self.stream = StreamLoader.LoadOGGFromBytes(bytes)
+		
+		file.close()
 	
-	# Load mp3
-	if music.ends_with(".mp3"):
-		self.stream = StreamLoader.LoadMP3FromBytes(bytes)
-	# Load wav
-	elif music.ends_with(".wav"):
-		self.stream = StreamLoader.LoadWAVFromBytes(bytes)
-	# Load wav
-	elif music.ends_with(".ogg"):
-		self.stream = StreamLoader.LoadOGGFromBytes(bytes)
-	
-	file.close()
+	else:
+		self.stream = AudioStreamFLAC.load_from_file(music)
 	
 	self.play()
 	
